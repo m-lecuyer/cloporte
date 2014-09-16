f65e56ded41b996fcbba852059a1414ece563a2e
Mathias Lecuyer <mathias@lecuyer.me>
2014-09-15 21:50:29 -0400

    started to implement redis queue

--- src/cloporte/job_queue.clj
+++ src/cloporte/job_queue.clj
@@ -1,20 +1,15 @@
 (ns cloporte.job-queue
   (:require [clojure.data.json :as json]
-            [taoensso.carmine :as car :refer (wcar)]))
+            [taoensso.carmine :as redis]
+            [taoensso.carmine.message-queue :as redis-mq]))
+
+(defmacro wcar* [& body] `(redis/wcar nil ~@body))
 
 (defn enqueue-job
   "Queues a job in Redis for it to be consumed by a worker."
   [options job]
-  (let [queue (or (:queue options) :default)
-        job-json (json/write-str job)]  ;; TODO other options: {:after time}...
-                                               ;; TODO centralize defaults
+  ;; TODO other options: {:after time}...
+  ;; TODO centralize defaults
+  (let [queue (or (:queue options) :default)]
     ;; here we put the job in a redis queue
-    (println "queue: " queue)
-    (println "job: " job-json)
-    (println "options: " options)))
-
-;; TODO don't transform stings to keywords in args?
-(defn unmarshal
-  "Desirializes the job-json."
-  [job-json]
-  (json/read-str job-json :key-fn keyword))
+    (wcar* (redis-mq/enqueue (str queue) job))))
---
+++ src/cloporte/worker.clj
@@ -0,0 +1,17 @@
+(ns cloporte.worker
+  (:require [taoensso.carmine.message-queue :as redis-mq]
+            [cloporte.serializer :as s]))
+
+;; remember it to close it
+;; TODO keep it somewhere to close all ?
+(defn start-worker
+  "Starts a worker with n threads, on a given queue."
+  ([nthreads] (start-worker nthreads :default))
+  ([nthreads queue]
+   (redis-mq/worker nil (or queue :default)
+                    {:handler (fn [{:keys [message attempt]}]
+                                (s/perform-job message)
+                                {:status :success})
+                     :nthreads 5})))
+
+; (redis-mq/stop my-worker)
